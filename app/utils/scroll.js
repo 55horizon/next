ae4cd4a9de70d468f6213db57afd1301244f45ee
Sam Larsen <sam@slarsen.io>
2024-01-12 01:44:47 -0500

    Simplify scroll

--- app/utils/scroll.js
+++
@@ -1,9 +0,0 @@
-export default function Scroll(e, href, offset) {
-	e.preventDefault();
-
-	const element = document.querySelector(href);
-	if (!element) return;
-
-	const top = element.getBoundingClientRect().top + window.scrollY - offset;
-	window.scrollTo({ top, behavior: "smooth" });
-}
